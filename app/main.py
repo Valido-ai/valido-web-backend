d7a42f1b20e1b50b587c204198423e25bd4b0c60
nmv14 <nikileshmula14@gmail.com>
2025-09-13 14:44:45 +0530

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import waitlist, notify
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Valido Waitlist API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # during development, allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routes
 app.include_router(waitlist.router, prefix="/waitlist", tags=["Waitlist"])
